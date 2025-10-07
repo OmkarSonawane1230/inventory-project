e2715d93392ad014249e6fef1bd62f98ada04269
Omkar Sonawane <163371960+OmkarSonawane1230@users.noreply.github.com>
2025-10-07 03:06:46 +0000

    deleted:    gitignore.txt 	modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,6 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './attached_assets'),
-    };
-    return config;
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
+  
 };
 
 export default nextConfig;
